40ab56c14a2883716a95af05d33ce8c9d7cdb511
Polykek2K <45636036+Polykek2K@users.noreply.github.com>
2025-12-27 20:45:42 +0300

    Апдейт для пересборки

--- KirstGrab.py
+++ KirstGrab.py
@@ -99,7 +99,7 @@ def paste_cookies():
 
 
 # Current version - update this when releasing new versions
-CURRENT_VERSION = "1.3.16"
+CURRENT_VERSION = "1.3.17"
 GITHUB_REPO = "Polykek2K/KirstGrab"
 
 
