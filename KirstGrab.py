2d563b479d39e0b6fa29487a1ab1c47dd5b9794b
Polykek2K <45636036+Polykek2K@users.noreply.github.com>
2026-04-17 00:13:59 +0300

    Куки используется только для видео

--- KirstGrab.py
+++ KirstGrab.py
@@ -99,7 +99,7 @@ def paste_cookies():
 
 
 # Current version - update this when releasing new versions
-CURRENT_VERSION = "1.4.7"
+CURRENT_VERSION = "1.4.8"
 GITHUB_REPO = "Polykek2K/KirstGrab"
 
 
@@ -551,7 +551,7 @@ def build_command(url, download_path, format_choice, filename=None):
     cookies_path = resource_path("cookies.txt")
     ensure_cookies_file(cookies_path)
     # Only use cookies if the file is not empty
-    if os.path.getsize(cookies_path) > 0:
+    if os.path.getsize(cookies_path) > 0 and format_choice == "Best Quality (MP4):
         cmd.extend(["--cookies", cookies_path])
     
     # Handle custom filename if provided and not empty
