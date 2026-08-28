28c99f584eb53d7a46f32ce57a4de9a1d09c3a36
Polykek2K <poddubik1967@gmail.com>
2026-08-28 18:34:47 +0300

    Поднята версия

--- KirstGrab.py
+++ KirstGrab.py
@@ -133,7 +133,7 @@ def paste_cookies():
 
 
 # Current version - update this when releasing new versions
-CURRENT_VERSION = "1.6.5"
+CURRENT_VERSION = "1.6.6"
 GITHUB_REPO = "Polykek2K/KirstGrab"
 
 
